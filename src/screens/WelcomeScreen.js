b86a9c824f242ed3e467a628f1a173ab7f8ae245
benjamim015 <benjamimq@hotmail.com>
2020-07-27 02:30:30 -0400

    Fix WelcomeScreen

--- src/screens/WelcomeScreen.js
+++ src/screens/WelcomeScreen.js
@@ -6,28 +6,52 @@ import styled from 'styled-components';
 
 const screenWidth = Dimensions.get('window').width;
 const screenHeight = Dimensions.get('window').height;
+
 const WelcomeScreen = ({ navigation }) => {
   return (
     <ImageBackground
       source={WSBackground}
       style={{ width: '100%', height: '100%' }}>
-      <Text1>BEM-VINDO</Text1>
-      <Text2>é bom ter você conosco</Text2>
-      <StartButton>
-        <Text3>Começar</Text3>
-      </StartButton>
-      <View style={{ flexDirection: 'row', flex: 1, alignItems: 'flex-end' }}>
+      <View style={{ height: '50%' }}>
+        <View
+          style={{
+            // backgroundColor: 'purple',
+            marginTop: 50,
+            flexDirection: 'row-reverse',
+          }}>
+          <View style={{ flexDirection: 'column' }}>
+            <Text1>BEM-VINDO</Text1>
+            <Text2>é bom ter você conosco</Text2>
+          </View>
+          {/* <LogoImage source={WSLogo}></LogoImage> */}
+        </View>
+        <View
+          style={{
+            justifyContent: 'flex-end',
+            alignItems: 'flex-end',
+            marginTop: 180,
+            width: '100%',
+          }}>
+          <StartButton onPress={() => navigation.navigate('Login')}>
+            <Text3>Começar</Text3>
+          </StartButton>
+        </View>
+      </View>
+      <View
+        style={{
+          flexDirection: 'row',
+          alignItems: 'flex-end',
+          // backgroundColor: 'black',
+          flex: 1,
+          alignSelf: 'flex-start',
+          marginBottom: 10,
+          marginLeft: 10,
+        }}>
         <Text4>Ainda não tem uma conta?</Text4>
         <RegisterButton onPress={() => navigation.navigate('Register')}>
-          <Text5>Registre-se</Text5>
+          <Text5>Registre-se!</Text5>
         </RegisterButton>
       </View>
-      <LogoImage
-        source={WSLogo}
-        style={{
-          position: 'absolute',
-          resizeMode: 'contain',
-        }}></LogoImage>
     </ImageBackground>
   );
 };
@@ -35,35 +59,38 @@ const WelcomeScreen = ({ navigation }) => {
 const Text1 = styled.Text`
   font-family: Kanit-Regular;
   color: #000000;
-  font-size: 40;
-  top: 2.5%;
-  left: 4.7%;
+  font-size: 50;
+  align-self: flex-end;
+  margin-right: 20;
 `;
 
 const Text2 = styled.Text`
-  font-size: 18.5;
-  top: 0.7%;
+  font-size: 20;
   color: #000000;
-  left: 5.4%;
   font-family: Kanit-Regular;
+  align-self: flex-end;
+  margin-top: -15;
+  margin-right: 22;
 `;
 
 const Text3 = styled.Text`
-  font-family: Kanit-Regular;
-  font-size: 18;
   color: #ffffff;
+  font-size: 20;
+  font-family: Kanit-Regular;
 `;
 
 const Text4 = styled.Text`
   font-family: Kanit-Regular;
   color: #ffffff;
-  font-size: 15.8;
+  font-size: 16;
 `;
 
 const Text5 = styled.Text`
   font-family: Kanit-Regular;
-  font-size: 15.8;
+  font-size: 18;
   color: #0000ff;
+  margin-left: -15;
+  margin-top: 3;
 `;
 
 const RegisterButton = styled.TouchableOpacity`
@@ -74,21 +101,18 @@ const RegisterButton = styled.TouchableOpacity`
 `;
 
 const StartButton = styled.TouchableOpacity`
-  background-color: #000000;
-  width: ${screenWidth * 0.4};
-  height: ${screenHeight * 0.05};
+  background-color: #0f1218;
+  width: ${screenHeight * 0.25};
+  height: ${screenHeight * 0.065};
+  border-radius: 100;
   justify-content: center;
   align-items: center;
-  border-radius: 35;
-  left: 57%;
-  top: 28%;
+  margin-right: 20;
 `;
 
 const LogoImage = styled.Image`
   width: ${screenWidth * 0.4};
   height: ${screenHeight * 0.075};
-  top: 5%;
-  right: -1%;
 `;
 
 export default WelcomeScreen;
