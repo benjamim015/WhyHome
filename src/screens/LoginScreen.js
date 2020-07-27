5f1732ebb7843935f0d849ce671486ce190778fb
benjamim015 <benjamimq@hotmail.com>
2020-07-27 02:53:34 -0400

    Fix LoginScreen

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,85 +1,83 @@
 import React, { useState } from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-  Dimensions,
-  Image,
-  ImageBackground,
-  TextInput,
-} from 'react-native';
+import { View, Dimensions, ImageBackground } from 'react-native';
+
 import styled from 'styled-components';
 import LSBackground from '../assets/loginscreen.png';
 import LSPeople from '../assets/loginscreenpeople.png';
 
 const screenWidth = Dimensions.get('window').width;
 const screenHeight = Dimensions.get('window').height;
+
 const LoginScreen = ({ navigation }) => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const login = () => {
+    navigation.navigate('WhyHome');
+  };
+
   return (
     <ImageBackground
       source={LSBackground}
       style={{ width: '100%', height: '100%' }}>
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Text1>WHY HOME</Text1>
+      <View
+        style={{
+          flex: 1,
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}>
         <PeopleImg source={LSPeople}></PeopleImg>
         <Container>
-          <InputEmail placeholder="Email"></InputEmail>
-          <InputPassword
+          <StyledTextInput placeholder="Email"></StyledTextInput>
+          <StyledTextInput
             placeholder="Senha"
-            secureTextEntry={true}></InputPassword>
+            secureTextEntry={true}></StyledTextInput>
+          <LoginButton onPress={login}>
+            <String>Login</String>
+          </LoginButton>
         </Container>
-        <LoginButton onPress={() => navigation.navigate('WhyHome')}>
-          <String>Login</String>
-        </LoginButton>
       </View>
     </ImageBackground>
   );
 };
 
 const PeopleImg = styled.Image`
-  width: ${screenWidth * 0.4};
-  height: ${screenHeight * 0.2};
-`;
-const Text1 = styled.Text`
-  font-size: 40;
-  font-family: Kanit-Regular;
+  width: 150;
+  height: 150;
+  align-self: center;
+  margin-top: 20;
+  margin-bottom: 20;
+  /* resize-mode: contain; */
+  /* background-color: red; */
 `;
 
-const Container = styled.View``;
+const Container = styled.View`
+  width: ${screenWidth * 0.7};
+  height: ${screenHeight * 0.5};
+`;
 
-const InputEmail = styled.TextInput`
-  background-color: rgba(255, 255, 255, 0.3);
+const StyledTextInput = styled.TextInput`
+  background-color: rgba(255, 255, 255, 0.2);
   border-color: #cccccc;
-  border-width: 1;
-  border-radius: 16;
-  width: ${screenWidth * 0.55};
-  height: ${screenHeight * 0.06};
-  font-family: Kanit-Regular;
+  border-width: 2;
+  border-radius: 15;
+  height: ${screenHeight * 0.065};
   margin-bottom: 10;
-`;
-
-const InputPassword = styled.TextInput`
-  background-color: rgba(255, 255, 255, 0.3);
-  border-color: #cccccc;
-  border-width: 1;
-  border-radius: 16;
-  margin: 0 auto;
-  width: ${screenWidth * 0.55};
-  height: ${screenHeight * 0.06};
+  font-size: 20;
+  color: #0f1218;
+  padding-left: 15;
   font-family: Kanit-Regular;
-  margin-bottom: 10;
 `;
 
 const LoginButton = styled.TouchableOpacity`
-  width: ${screenWidth * 0.3};
-  height: ${screenHeight * 0.04};
-  border-radius: 16;
-  background-color: #000000;
-  justify-content: center;
+  background-color: #0f1218;
+  width: ${screenWidth * 0.5};
+  height: ${screenHeight * 0.065};
+  border-radius: 100;
+  margin-top: 20;
+  align-self: center;
   align-items: center;
+  justify-content: center;
 `;
 
 const String = styled.Text`
